36f4f8ab630cfaaf8cc4b196df8df5bc1dfc8a95
Joao Loula <joao.campos-loula@polytechnique.edu>
2016-05-20 18:04:50 +0200

    added report

--- mnist/model/train_mnist_siamese.sh
+++ mnist/model/train_mnist_siamese.sh
@@ -2,5 +2,5 @@
 
 TOOLS=../../../caffe-master/build/tools
 
-mpirun -np 8 $TOOLS/caffe time -model mnist_siamese_train_test.prototxt -iterations 10
-# $TOOLS/caffe train --solver=mnist_siamese_solver.prototxt | tee log_mnist.log
+# mpirun -np 4 $TOOLS/caffe time -model mnist_siamese_train_test.prototxt -iterations 10
+$TOOLS/caffe train --solver=mnist_siamese_solver.prototxt 2>&1 | tee log_mnist.log
